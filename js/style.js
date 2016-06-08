9df09d499725a97bfb03dba08a8d36ffa4338b78
Leonid <leoyats@gmail.com>
2016-06-08 12:26:01 +0000

    replace tags with points сss js

--- js/style.js
+++ js/style.js
@@ -1,18 +1,18 @@
 jQuery(document).ready(function($) {
-	var images = ["Layer 38.png", "Layer 39.png"];
-	var currentImage = 0;
-	jQuery("#img").attr('src', 'images[currentImage]');
-	jQuery("#up").click(function {
-		if (currentImage !=0) {
-			currentImage --;
-			Query("#img").attr('src', 'images[currentImage]');
-		}
-	});
-	jQuery("#down").click(function {
-		if (currentImage != images.length - 1) {
-			currentImage ++;
-			Query("#img").attr('src', 'images[currentImage]');
-		}
-	});
-	});
+    var images = ["Layer 38.png", "Layer 39.png"];
+    var currentImage = 0;
+    jQuery("#img").attr('src', 'images[currentImage]');
+    jQuery("#up").click(function {
+        if (currentImage !=0) {
+            currentImage --;
+            Query("#img").attr('src', 'images[currentImage]');
+        }
+    });
+    jQuery("#down").click(function {
+        if (currentImage != images.length - 1) {
+            currentImage ++;
+            Query("#img").attr('src', 'images[currentImage]');
+        }
+    });
+    });
 });
