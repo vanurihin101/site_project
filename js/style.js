e5c8f1b0679fd8d4d3d120d5c773e159bc50c4b9
Leonid <leoyats@gmail.com>
2016-06-08 12:47:58 +0000

    fix js

--- js/style.js
+++ js/style.js
@@ -1,18 +1,17 @@
-jQuery(document).ready(function($) {
+$(document).ready(function() {
     var images = ["Layer 38.png", "Layer 39.png"];
     var currentImage = 0;
-    jQuery("#img").attr('src', 'images[currentImage]');
-    jQuery("#up").click(function {
-        if (currentImage !=0) {
-            currentImage --;
-            Query("#img").attr('src', 'images[currentImage]');
+    $("#img").attr('src', images[currentImage]);
+    $("#up").click(function (){
+        if (currentImage !== 0) {
+            currentImage--;
+            $("#img").attr('src', images[currentImage]);
         }
     });
-    jQuery("#down").click(function {
+    $("#down").click(function (){
         if (currentImage != images.length - 1) {
-            currentImage ++;
-            Query("#img").attr('src', 'images[currentImage]');
+            currentImage++;
+            $("#img").attr('src', images[currentImage]);
         }
-    });
     });
 });
